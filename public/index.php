801a645dfea492be7ef47dd2c6d46a350a4ca414
AndersonCavalcanteBSI <anderson.cavalcante@bsicapital.com.br>
2025-09-10 17:08:46 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ use App\Middlewares\SecurityHeadersMiddleware;
 use App\Middlewares\CORSMiddleware;
 use App\Controllers\HomeController;
 use App\Controllers\OperationController;
+use App\Controllers\MeasurementController;
 
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -28,6 +29,8 @@ $router->get('/', fn() => (new HomeController())->index());
 $router->get('/operations', fn() => (new OperationController())->index());
 $router->get('/operations/{id}', fn(string $id) => (new OperationController())->show((int)$id));
 $router->post('/measurements/{id}/analyze', fn(string $id) => (new \App\Controllers\OperationController())->analyzeFile((int)$id));
+$router->get('/measurements/upload', fn() => (new MeasurementController())->create());
+$router->post('/measurements/upload', fn() => (new MeasurementController())->store());
 
 
 $router->dispatch();

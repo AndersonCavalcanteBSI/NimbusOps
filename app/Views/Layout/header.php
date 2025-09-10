2a7e3ecb2ffc9c250ad308325c45b46a26c02f9d
AndersonCavalcanteBSI <anderson.cavalcante@bsicapital.com.br>
2025-09-10 17:08:53 -0300

    Update header.php

--- app/Views/Layout/header.php
+++ app/Views/Layout/header.php
@@ -18,6 +18,7 @@
             <div class="collapse navbar-collapse" id="nav">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item"><a class="nav-link" href="/operations">Operações</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/measurements/upload">Medições</a></li>
                 </ul>
             </div>
         </div>

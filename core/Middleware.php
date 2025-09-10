1c22f404a177f4e9c599a5bbae4ec8dcfd1b3db3
AndersonCavalcanteBSI <anderson.cavalcante@bsicapital.com.br>
2025-09-10 14:22:48 -0300

    Update Middleware.php

--- core/Middleware.php
+++ core/Middleware.php
@@ -1,14 +1,10 @@
 <?php
-declare(strict_types=1);
 
+declare(strict_types=1);
 
 namespace Core;
 
-
-type MiddlewareHandler = callable;
-
-
 interface Middleware
 {
-public function handle(): void;
+    public function handle(): void;
 }

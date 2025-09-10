69ce87da7bc9141b1a8a6aea20e1b035aecedc45
AndersonCavalcanteBSI <anderson.cavalcante@bsicapital.com.br>
2025-09-10 12:24:49 -0300

    Create User.php

---
+++ app/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Models;
+
+
+final class User
+{
+    public int $id;
+    public ?string $entra_object_id = null; // para mapear usuários do Entra ID
+    public string $name;
+    public string $email;
+    public ?string $avatar = null;
+    public bool $active = true;
+    public string $created_at;
+}

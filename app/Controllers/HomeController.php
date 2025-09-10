af4ff98870396d79cee4e6d5fb20d608cb5b7291
AndersonCavalcanteBSI <anderson.cavalcante@bsicapital.com.br>
2025-09-10 12:28:46 -0300

    Create HomeController.php

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Controllers;
+
+
+use Core\Controller;
+
+
+final class HomeController extends Controller
+{
+    public function index(): void
+    {
+        $this->view('home/index');
+    }
+}

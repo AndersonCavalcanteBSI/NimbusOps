9711918ec446a53bf117e171bf94404692f5de0a
AndersonCavalcanteBSI <anderson.cavalcante@bsicapital.com.br>
2025-09-10 12:21:48 -0300

    Create Response.php

---
+++ core/Response.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Core;
+
+
+final class Response
+{
+    public static function json(array $data, int $status = 200): void
+    {
+        http_response_code($status);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PARTIAL_OUTPUT_ON_ERROR);
+    }
+}
